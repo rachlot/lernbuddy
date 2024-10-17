03603dcd3fb71bb0140cb41807daf21fa6d86d90
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2024-10-17 10:25:20 +0000

    remove query

--- model/dj-query-catalog/LernzieleMitVerbesserungsbedarf.0.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  "Lernziel"."Fachbereich" AS "Fachbereich", "Lernziel"."Ziel" AS "Lernziel", AVG("Ergebnis"."prozent") AS "Durchschnittlicher Prozentwert"
-FROM
-  "Ergebnis"
-    INNER JOIN "Lernziel" ON "Ergebnis"."lernziel" = "Lernziel"."ID"
-GROUP BY
-   "Lernziel"."Fachbereich", "Lernziel"."Ziel"
-HAVING
-  AVG("Ergebnis"."prozent") < 60
